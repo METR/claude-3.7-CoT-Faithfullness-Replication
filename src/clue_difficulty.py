9b9a39383e1395966bd1fe600cf54273e6714e90
Sudarsh Kunnavakkam <kvsudarsh786@gmail.com>
2025-04-10 22:24:17 -0700

    benchmarking difficulties

--- src/clue_difficulty.py
+++ src/clue_difficulty.py
@@ -7,6 +7,7 @@ from inspect_ai.scorer import choice
 from behaviors import Behavior, FUNCTION_DICT
 from inspect_ai.model import GenerateConfig
 import matplotlib.pyplot as plt
+from tqdm import tqdm
 
 """
 Plan:
@@ -50,6 +51,7 @@ def clue_difficulty(behavior: str, reasoning: bool = False) -> float:
             reasoning_tokens=reasoning_tokens,
             max_tokens=32768,
         ),
+        epochs=5,
     )
 
     ...
@@ -63,8 +65,28 @@ model_pairs: List[Tuple[str, str]] = [  # reasoning model, non-reasoning model
 
 if __name__ == "__main__":
     models = model_pairs[0]
+    reasoning_results = []
+    non_reasoning_results = []
 
-    for behavior in FUNCTION_DICT.keys():
-        eval(clue_difficulty(behavior, reasoning=True), model=models[0])
-        eval(clue_difficulty(behavior, reasoning=False), model=models[1])
-    ...
+    for behavior in tqdm(FUNCTION_DICT.keys()):
+        reasoning_evalscore = eval(
+            clue_difficulty(behavior, reasoning=True), model=models[0]
+        )
+        non_reasoning_evalscore = eval(
+            clue_difficulty(behavior, reasoning=False), model=models[1]
+        )
+        reasoning_results.append(
+            reasoning_evalscore[0].results.scores[0].metrics["accuracy"].value
+        )
+        non_reasoning_results.append(
+            non_reasoning_evalscore[0].results.scores[0].metrics["accuracy"].value
+        )
+
+    delta = [a - b for a, b in zip(reasoning_results, non_reasoning_results)]
+
+    # Sort behaviors by delta value
+    sorted_results = sorted(
+        zip(FUNCTION_DICT.keys(), delta), key=lambda x: x[1], reverse=True
+    )
+    for behavior, delta in sorted_results:
+        print(f"{behavior}: {delta}")
