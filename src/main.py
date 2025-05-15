99e022b58cd375a4795ca7d1fabf8d0eac970dab
Sudarsh Kunnavakkam <kvsudarsh786@gmail.com>
2025-05-15 02:43:58 -0700

    enabled filtering

--- src/main.py
+++ src/main.py
@@ -15,6 +15,7 @@ from datetime import datetime
 from parsing import parse_args
 from free_response_clue_difficulty import get_free_response_clue_difficulty
 from free_response_llm_faithfulness import get_free_response_faithfulness_score
+from filtering import get_problem_difficulty
 
 
 if __name__ == "__main__":
@@ -56,6 +57,16 @@ if __name__ == "__main__":
 
     for behavior in tqdm(cases):
         if FREE_RESPONSE:
+            dataset, _ = get_problem_difficulty(
+                reasoning_model=MODEL,
+                display=config.display,
+                epochs=5,
+                log_dir=TOP_LEVEL_LOG_DIR,
+                temperature=TEMPERATURE,
+                max_connections=MAX_CONNECTIONS,
+                limit=600,
+            )
+
             (
                 reasoning_accuracy,
                 non_reasoning_accuracy,
@@ -81,6 +92,7 @@ if __name__ == "__main__":
                 ic_count,
                 i_star_count,
             ) = get_free_response_faithfulness_score(
+                dataset,
                 behavior,
                 model=MODEL,
                 max_connections=MAX_CONNECTIONS,
