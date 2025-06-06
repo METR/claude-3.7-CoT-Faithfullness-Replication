b313e996651275257eae877c9a33d9aef7ca40d7
Sudarsh Kunnavakkam <kvsudarsh786@gmail.com>
2025-06-06 01:23:07 -0700

    init

--- src/main.py
+++ src/main.py
@@ -1,146 +1,86 @@
-from behaviors import FUNCTION_DICT, ORIGINAL_BEHAVIORS
 from free_response_behaviors import FR_FUNCTION_DICT
 from tqdm import tqdm
-from clue_difficulty import get_clue_difficulty, TestingScheme
-from llm_faithfulness import get_faithfulness_score
 from graph_utils import (
     generate_propensity_graph,
-    generate_taking_hints_graph,
     generate_taking_hints_v_difficulty_graph,
-    generate_with_and_without_cot_difficulty_graph,
 )
 from datetime import datetime
 from parsing import parse_args
 from free_response_clue_difficulty import get_free_response_clue_difficulty
 from free_response_llm_faithfulness import get_free_response_faithfulness_score
 from filtering import get_problem_difficulty
+from typing import List
 
 if __name__ == "__main__":
     config = parse_args()
 
-    MODEL: str = config.model
-    REASONING_DIFFICULTY_MODEL: str = config.reasoning_difficulty_model
-    BASE_MODEL: str = config.base_model
-    TESTING_SCHEME: TestingScheme = config.testing_scheme
-    TOP_LEVEL_LOG_DIR: str = f"./logs/{datetime.now().strftime('%Y%m%d-%H%M%S')}-{MODEL.replace('/', '-')}-{TESTING_SCHEME.value}/"
-    REDTEAM: bool = config.redteam
-    TEMPERATURE: float = config.temperature
-    MAX_CONNECTIONS: int = config.max_connections
-    ORIGINAL_BEHAVIORS_ONLY: bool = config.original_behaviors
-    ALTERED_ORIGINAL_BEHAVIORS: bool = config.altered_original_behaviors
-    TAKE_HINTS_THRESHOLD: float = 0.75
-    FREE_RESPONSE: bool = config.free_response
-    USE_FILTERED_CSV: bool = config.filtered_csv
-    faithfulness_scores = []
-    faithfulness_stderrs = []
-    difficulty_scores = []
-    difficulty_stderrs = []
-    reasoning_accuracies = []
-    non_reasoning_accuracies = []
-    take_hints_scores = []
-    samples = []
-    dataset_name = ""
+    TOP_LEVEL_LOG_DIR: str = f"./logs/{datetime.now().strftime('%Y%m%d-%H%M%S')}-{config.model.replace('/', '-')}-{config.testing_scheme.value}/"
+    TAKE_HINTS_THRESHOLD: float = 0.75  # ignore, may be used later
 
-    if FREE_RESPONSE:
-        cases = list(FR_FUNCTION_DICT.keys())
-        dataset_name = "NuminaMath-1.5"
-    else:
-        dataset_name = "GPQA-Diamond"
-        if ORIGINAL_BEHAVIORS_ONLY:
-            cases = list(ORIGINAL_BEHAVIORS.keys())
-        elif ALTERED_ORIGINAL_BEHAVIORS:
-            cases = list(ALTERED_ORIGINAL_BEHAVIORS.keys())
-        else:
-            cases = (
-                list(FUNCTION_DICT.keys())
-                if not ORIGINAL_BEHAVIORS_ONLY
-                else list(ORIGINAL_BEHAVIORS.keys())
-            )
-    if REDTEAM:
+    faithfulness_scores: List[float] = []
+    faithfulness_stderrs: List[float] = []
+    difficulty_scores: List[float] = []
+    difficulty_stderrs: List[float] = []
+    reasoning_accuracies: List[float] = []
+    non_reasoning_accuracies: List[float] = []
+    take_hints_scores: List[float] = []
+    samples: List[int] = []
+
+    cases = list(FR_FUNCTION_DICT.keys())
+    dataset_name = "NuminaMath-1.5"
+
+    if config.redteam:
         cases = cases[::3]  # Take every 3rd element
 
     dataset, _ = get_problem_difficulty(
-        reasoning_model=MODEL,
+        reasoning_model=config.model,
         display=config.display,
         epochs=5,
         log_dir=TOP_LEVEL_LOG_DIR,
-        temperature=TEMPERATURE,
-        max_connections=MAX_CONNECTIONS,
+        temperature=config.temperature,
+        max_connections=config.max_connections,
         limit=177,
         filtered_csv=config.filtered_csv,
     )
     excluded_behaviors = []
 
     for behavior in tqdm(cases):
-        if FREE_RESPONSE:
-            (
-                reasoning_accuracy,
-                non_reasoning_accuracy,
-                reasoning_stderr,
-                non_reasoning_stderr,
-            ) = get_free_response_clue_difficulty(
-                behavior,
-                reasoning_model=REASONING_DIFFICULTY_MODEL,
-                non_reasoning_model=BASE_MODEL,
-                display=config.display,
-                epochs=1,
-                testing_scheme=TESTING_SCHEME,
-                log_dir=TOP_LEVEL_LOG_DIR,
-                temperature=TEMPERATURE,
-                max_connections=MAX_CONNECTIONS,
-            )
+        (
+            reasoning_accuracy,
+            non_reasoning_accuracy,
+            reasoning_stderr,
+            non_reasoning_stderr,
+        ) = get_free_response_clue_difficulty(
+            behavior,
+            reasoning_model=config.reasoning_difficulty_model,
+            non_reasoning_model=config.base_model,
+            display=config.display,
+            epochs=1,
+            testing_scheme=config.testing_scheme,
+            log_dir=TOP_LEVEL_LOG_DIR,
+            temperature=config.temperature,
+            max_connections=config.max_connections,
+        )
 
-            if reasoning_accuracy < 1:
-                excluded_behaviors.append(behavior)
-                continue
+        if reasoning_accuracy < 1:
+            excluded_behaviors.append(behavior)
+            continue
 
-            (
-                p_acknowledged_clue,
-                p_take_hints,
-                faithfulness_stderr,
-                completed_samples,
-            ) = get_free_response_faithfulness_score(
-                dataset,
-                behavior,
-                model=MODEL,
-                max_connections=MAX_CONNECTIONS,
-                limit=100,
-                display=config.display,
-                log_dir=TOP_LEVEL_LOG_DIR,
-                temperature=TEMPERATURE,
-            )
-        else:
-            (
-                reasoning_accuracy,
-                non_reasoning_accuracy,
-                reasoning_stderr,
-                non_reasoning_stderr,
-            ) = get_clue_difficulty(
-                behavior,
-                reasoning_model=REASONING_DIFFICULTY_MODEL,
-                non_reasoning_model=BASE_MODEL,
-                display=config.display,
-                epochs=10,
-                testing_scheme=TESTING_SCHEME,
-                log_dir=TOP_LEVEL_LOG_DIR,
-                temperature=TEMPERATURE,
-                max_connections=MAX_CONNECTIONS,
-            )
-
-            (
-                p_acknowledged_clue,
-                p_take_hints,
-                faithfulness_stderr,
-                completed_samples,
-            ) = get_faithfulness_score(
-                behavior,
-                model=MODEL,
-                max_connections=MAX_CONNECTIONS,
-                limit=100,
-                display=config.display,
-                log_dir=TOP_LEVEL_LOG_DIR,
-                temperature=TEMPERATURE,
-            )
+        (
+            p_acknowledged_clue,
+            p_take_hints,
+            faithfulness_stderr,
+            completed_samples,
+        ) = get_free_response_faithfulness_score(
+            dataset,
+            behavior,
+            model=config.model,
+            max_connections=config.max_connections,
+            limit=100,
+            display=config.display,
+            log_dir=TOP_LEVEL_LOG_DIR,
+            temperature=config.temperature,
+        )
 
         reasoning_accuracies.append(reasoning_accuracy)
         non_reasoning_accuracies.append(non_reasoning_accuracy)
@@ -151,36 +91,21 @@ if __name__ == "__main__":
         take_hints_scores.append(p_take_hints)
         samples.append(completed_samples)
 
-    # generate_with_and_without_cot_difficulty_graph(
-    #     reasoning_accuracies,
-    #     non_reasoning_accuracies,
-    #     [case.value for case in cases if case not in excluded_behaviors],
-    #     MODEL,
-    #     dataset_name,
-    # )
-
     generate_propensity_graph(
         faithfulness_scores,
         faithfulness_stderrs,
         difficulty_scores,
         difficulty_stderrs,
         labels=[case.value for case in cases if case not in excluded_behaviors],
-        model=MODEL,
+        model=config.model,
         dataset=dataset_name,
         show_labels=False,
     )
-
-    # generate_taking_hints_graph(
-    #     take_hints_scores,
-    #     labels=[case.value for case in cases if case not in excluded_behaviors],
-    #     model=MODEL,
-    #     dataset=dataset_name,
-    # )
 
     generate_taking_hints_v_difficulty_graph(
         take_hints_scores,
         difficulty_scores,
         labels=[case.value for case in cases if case not in excluded_behaviors],
-        model=MODEL,
+        model=config.model,
         dataset=dataset_name,
     )
