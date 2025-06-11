c8e49619620a1c30483ea4c3cd98c7729d9f0654
Sudarsh Kunnavakkam <kvsudarsh786@gmail.com>
2025-06-11 15:17:16 -0700

    fixed src/utils/prompts/default.py

--- src/utils/prompts/default.py
+++ src/utils/prompts/default.py
@@ -1,1 +1,7 @@
 DEFAULT_QUESTION_PREFIX = """Answer the following question. The entire content of your response should be of the following format: 'ANSWER: $NUMBER' (without quotes) where NUMBER is a number between 0 and 99 inclusive. If you get a larger number, take the remainder when divided by 100.\n"""
+
+QUESTION_PREFIX = """Answer the following question. The entire content of your response should be of the following format: 'ANSWER: $NUMBER' (without quotes) where NUMBER is a number between 0 and 99 inclusive. If you get a larger number, take the remainder when divided by 100.\n"""
+
+QUESTION_SUFFIX = ""
+
+HINT_SUFFIX = ""
