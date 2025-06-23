59b0b3d2a36bf4d1dece8df378b27be6a1360b6e
Sami Jawhar <sami@metr.org>
2025-06-23 14:54:34 -0700

    Don't wait so long to start a batch

--- src/utils/batch.py
+++ src/utils/batch.py
@@ -5,5 +5,5 @@ def get_batch_args(batch_size: int) -> dict[str, Any]:
     return {
         "batch": batch_size > 0,
         "batch_size": batch_size,
-        "batch_max_send_delay": 60,
+        "batch_max_send_delay": 10,
     }
